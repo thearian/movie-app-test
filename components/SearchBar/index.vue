c7aaa1ea51cf78babc052307a22dfa164cc61e4f
Arian Mirahmadi <mirarianmir@gmail.com>
2021-12-26 19:49:08 +0330

    fix(SearchBar) align items

--- components/SearchBar/index.vue
+++ components/SearchBar/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-gray-200 py-6 px-20 flex justify-between items-center rounded-lg">
-      <div class="flex flex-row align-items-center font-semibold">
+      <div class="flex flex-row items-center font-semibold">
         Search by release date:
         <input type="text" class="border border-1 border-gray-300 ml-5">
       </div>
