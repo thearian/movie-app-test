f4db6c2b6e464769c18f491133d177a342c62523
Arian Mirahmadi <mirarianmir@gmail.com>
2021-12-27 03:43:17 +0330

    feat: vue date picker

--- components/SearchBar/index.vue
+++ components/SearchBar/index.vue
@@ -2,14 +2,33 @@
     <div class="bg-gray-200 py-6 px-20 flex justify-between items-center rounded-lg">
       <div class="flex flex-row items-center font-semibold">
         Search by release date:
-        <input type="text" class="border border-1 border-gray-300 ml-5">
+        <date-picker
+          v-model="dateRange"
+          class="ml-5"
+          @update="updateDateRange"
+        ></date-picker>
       </div>
       <button class="bg-blue-400 rounded-full py-1 px-3 text-white">Search</button>
     </div>
 </template>
 
 <script>
+import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
+
 export default {
-  name: 'SearchBar'
+  name: 'SearchBar',
+  data () {
+    return {
+      dateRange: {
+        startDate: null,
+        endDate: null
+      }
+    }
+  },
+  methods: {
+    updateDateRange () {
+      console.log(this.dateRange)
+    }
+  }
 }
 </script>
